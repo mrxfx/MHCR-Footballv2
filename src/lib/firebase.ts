acbbb7db27e4e0f59666118598f612c51ca99d49
Mr.XF <mrxfzyz143@gmail.com>
2026-06-01 10:04:49 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC9oJ2azvuXaIM_5bxn4buDbAqIjR19CDU",
+  authDomain: "mhcrftbl.firebaseapp.com",
+  projectId: "mhcrftbl",
+  storageBucket: "mhcrftbl.firebasestorage.app",
+  messagingSenderId: "1036525734484",
+  appId: "1:1036525734484:web:18ea1219ebb0fd40c3bbc1",
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
